61bc321e488a041f32499c79dbb380ebc8673f63
Sergio V <vucket07@gmail.com>
2022-01-26 21:17:16 +0100

    Fix: Moving validation to block addition fn

--- Project1/src/blockchain.js
+++ Project1/src/blockchain.js
@@ -70,6 +70,10 @@ class Blockchain {
   _addBlock = (block) => {
     return new Promise(async (resolve, reject) => {
       try {
+        const errArray = await this.validateChain();
+        if (errArray.length > 0) {
+          throw new Error("Block chain is invalid, cannot add more blocks");
+        }
         const currentHeight = await this.getChainHeight();
         const newHeight = currentHeight + 1;
         if (currentHeight > -1) {
@@ -127,10 +131,6 @@ class Blockchain {
   submitStar = (address, message, signature, star) => {
     return new Promise(async (resolve, reject) => {
       try {
-        const errArray = await this.validateChain();
-        if (errArray.length > 0) {
-          throw new Error("Block chain is invalid, cannot add more blocks");
-        }
         const timeFromMsg = parseInt(message.split(":")[1]);
         const currentTime = parseInt(this.getTimeStampString());
         if (currentTime - timeFromMsg > 300) {
