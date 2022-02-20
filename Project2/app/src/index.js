dfbc4814d9aac5eeb61cf486a08bccda5ec293af
Sergio V <vucket07@gmail.com>
2022-02-20 18:57:12 +0100

    Feat: Adding main functionality

--- Project2/app/src/index.js
+++ Project2/app/src/index.js
@@ -6,7 +6,7 @@ const App = {
   account: null,
   meta: null,
 
-  start: async function() {
+  start: async function () {
     const { web3 } = this;
 
     try {
@@ -15,7 +15,7 @@ const App = {
       const deployedNetwork = starNotaryArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
         starNotaryArtifact.abi,
-        deployedNetwork.address,
+        deployedNetwork.address
       );
 
       // get accounts
@@ -26,38 +26,41 @@ const App = {
     }
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     const status = document.getElementById("status");
     status.innerHTML = message;
   },
 
-  createStar: async function() {
+  createStar: async function () {
     const { createStar } = this.meta.methods;
     const name = document.getElementById("starName").value;
     const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
+    await createStar(name, id).send({ from: this.account });
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
   // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
-  }
+  lookUp: async function () {
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
 
+    const starId = document.getElementById("name").value;
+    const res = await lookUptokenIdToStarInfo(starId).send({
+      from: this.account,
+    });
+    console.log("star query", res);
+    App.setStatus("Owner of star queried is " + res + ".");
+  },
 };
 
 window.App = App;
 
-window.addEventListener("load", async function() {
+window.addEventListener("load", async function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
+    await window.ethereum.enable();
+    App.start();
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    console.error("No Web3");
   }
-
-  App.start();
 });
