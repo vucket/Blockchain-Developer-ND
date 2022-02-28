57705f569c6726b1b65e91a2962ed7b0a0964895
Sergio V <vucket07@gmail.com>
2022-02-28 21:01:22 +0100

    Feat: Completing Ownable

--- Project5/eth-contracts/contracts/ERC721Mintable.sol
+++ Project5/eth-contracts/contracts/ERC721Mintable.sol
@@ -1,23 +1,44 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/utils/Address.sol";
-import "openzeppelin-solidity/contracts/drafts/Counters.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
+import "../../node_modules/openzeppelin-solidity/contracts/utils/Address.sol";
+import "../../node_modules/openzeppelin-solidity/contracts/drafts/Counters.sol";
+import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../../node_modules/openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
 import "./Oraclize.sol";
 
 contract Ownable {
     //  TODO's
     //  1) create a private '_owner' variable of type address with a public getter function
+    address private _owner;
+
+    function getOwner() public view returns (address) {
+        return _owner;
+    }
+
     //  2) create an internal constructor that sets the _owner var to the creater of the contract
+
+    constructor() internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(_owner);
+    }
+
     //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Calleris not owner.");
+        _;
+    }
+
     //  4) fill out the transferOwnership function
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != _owner, "New owner must be different");
+        require(newOwner != address(0), "Address must be real");
+        _owner = newOwner;
+        emit OwnershipTransferred(_owner);
+    }
+
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        // TODO add functionality to transfer control of the contract to a newOwner.
-        // make sure the new owner is a real address
-    }
+    event OwnershipTransferred(address newOwner);
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
